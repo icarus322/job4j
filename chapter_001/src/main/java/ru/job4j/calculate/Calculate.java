82733a6b4c75a4df78fca3584a234fa0793444c3
icarus322 <pirat18l@ya.ru>
2019-04-15 21:20:01 +0500

    1.3. JavaDoc. Code convention[#133744]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate.
+ *
+ *@author Sergey Polev (pirat18l@ya.ru)
+ */
 public class Calculate{
 
+/**
+ * Main.
+ * @param args - args
+ */
 public static void main (String[] args) {
 	System.out.print("Hello World");
 	}
