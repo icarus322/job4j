4cbe1f2361aeff779810a2c39b9c015398025d2e
icarus322 <pirat18l@ya.ru>
2019-04-18 22:02:12 +0500

    4.1. Глупый бот.[#133701]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+/**
+ * @author Sergey Polev (pirat18l@ya.ru)
+ * @version 1.0
+ * @since 18.04.2019
+ */
+
+public class DummyBot {
+
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
