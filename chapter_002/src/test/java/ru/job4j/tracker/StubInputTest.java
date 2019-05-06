404df4a6d10a8d35b1826c68b23f64acd07fd324
icarus322 <pirat18l@ya.ru>
2019-05-06 14:44:44 +0500

    3. Используя класс StubInput написать тесты проверяющие поведение пользователя[#133786] проблемы

--- chapter_002/src/test/java/ru/job4j/tracker/StubInputTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StubInputTest.java
@@ -1,6 +1,9 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -26,10 +29,17 @@ public class StubInputTest {
     @Test
     public void whenDeleteThenTrackerHasDeletedValue() {
         Tracker tracker = new Tracker();
-        Item first = tracker.add(new Item("first", "desc", System.currentTimeMillis()));
-        Item second = tracker.add(new Item("second", "desc", System.currentTimeMillis()));
-        Input input = new StubInput(new String[]{"3", first.getId(), "6"});
+        Item[] items = {
+                tracker.add(new Item("first", "desc", System.currentTimeMillis())),
+                tracker.add(new Item("third", "desc", System.currentTimeMillis())),
+                tracker.add(new Item("second", "desc", System.currentTimeMillis()))
+        };
+        Item[] result = new Item[] {items[0], items[2]};
+        Input input = new StubInput(new String[]{"3", items[1].getId(), "1", "6"});
         new StartUI(input, tracker).init();
-        assertThat(tracker.getItems()[0].getName(), is("second"));
+        System.out.println(items[0].getId());
+        System.out.println(items[1].getId());
+        System.out.println(items[2].getId());
+        assertThat(result, is(items));
     }
 }
