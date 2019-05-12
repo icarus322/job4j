b7639930817f6d41b9e9df1d0beccf1f72c69abc
icarus322 <pirat18l@ya.ru>
2019-05-12 11:09:23 +0500

    1. Обеспечить бесперебойную работу приложения Tracker.[#133792] итоговые исправления

--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -14,7 +14,19 @@ public class StubInput implements Input {
     public String ask(String question) {
         return this.value[this.position++];
     }
-    public int ask (String question, int[] range){
-        throw new UnsupportedOperationException("Unsupported operation");
+    public int ask (String question, int[] range) {
+        int key = Integer.valueOf(this.ask(question));
+        boolean exist = false;
+        for (int value : range) {
+            if (value == key) {
+                exist = true;
+                break;
+            }
+        }
+        if (exist) {
+            return key;
+        } else {
+            throw new MenuOutException("Out of menu range");
+        }
     }
 }
