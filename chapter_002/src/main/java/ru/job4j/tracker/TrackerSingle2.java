263cee140f451e7308cc87b4beeb4b7d7e32c18f
icarus322 <pirat18l@ya.ru>
2019-05-13 16:37:41 +0500

    1. Singleton.[#133809] todo

---
+++ chapter_002/src/main/java/ru/job4j/tracker/TrackerSingle2.java
@@ -0,0 +1,87 @@
+package ru.job4j.tracker;
+
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class TrackerSingle2 {
+
+    private Item[] items = new Item[100];
+    private static final Random RN = new Random();
+
+    private int position = 0;
+
+    /**
+     * Метод реализаущий добавление заявки в хранилище
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        this.items[this.position++] = item;
+        return item;
+    }
+
+    protected Item findById(String id) {
+        Item result = null;
+        for (Item item : items) {
+            if (item != null && item.getId().equals(id)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private String generateId() {
+        return String.valueOf(System.currentTimeMillis() + RN.nextInt());
+    }
+
+    public boolean replace(String id, Item item) {
+        boolean result = false;
+        item.setId(id);
+        for (int i = 0; i < this.position; i++) {
+            if (this.items[i].getId().equals(id)) {
+                this.items[i] = item;
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public boolean delete(String id) {
+        boolean result = false;
+        for (int i = 0; i < this.position; i++) {
+            if (this.items[i].getId().equals(id)) {
+                System.arraycopy(this.items, i + 1, this.items, i, position - 1 - i);
+                items[--position] = null;
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(this.items, this.position);
+    }
+
+    public Item[] findByName(String key) {
+        int number = 0;
+        Item[] temp = new Item[this.position];
+        for (int i = 0; i < this.position; i++) {
+            if (this.items[i].getName().equals(key)) {
+                temp[number++] = this.items[i];
+            }
+        }
+        return Arrays.copyOf(temp, number);
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public Item[] getItems() {
+        return this.items;
+    }
+}
