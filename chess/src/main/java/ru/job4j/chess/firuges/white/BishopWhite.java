358d1ba9d31334191838c1c44f3fb80ef6afb2a5
icarus322 <pirat18l@ya.ru>
2019-05-15 23:13:44 +0500

    Каркас шахматной доски[#133814]

---
+++ chess/src/main/java/ru/job4j/chess/firuges/white/BishopWhite.java
@@ -0,0 +1,48 @@
+package ru.job4j.chess.firuges.white;
+
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+/**
+ *
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class BishopWhite implements Figure {
+    private final Cell position;
+
+    public BishopWhite(final Cell position) {
+        this.position = position;
+    }
+
+    @Override
+    public Cell position() {
+        return this.position;
+    }
+
+    @Override
+    public Cell[] way(Cell source, Cell dest) {
+        Cell[] steps = new Cell[0];
+        for (int i = 0; i < 8; i++) {
+            if (source.y == dest.y - i && source.x == dest.x - i) {
+                steps = new Cell[] { dest };
+            }
+            if (source.y == dest.y + i && source.x == dest.x + i) {
+                steps = new Cell[] { dest };
+            }
+            if (source.y == dest.y - i && source.x == dest.x + i) {
+                steps = new Cell[] { dest };
+            }
+            if (source.y == dest.y + i && source.x == dest.x - i) {
+                steps = new Cell[] { dest };
+            }
+        }
+        return steps;
+    }
+
+    @Override
+    public Figure copy(Cell dest) {
+        return new BishopWhite(dest);
+    }
+}
