7bd9c5f65052a3eb6db0bbf736e23fd7b86e4485
icarus322 <pirat18l@ya.ru>
2019-07-11 14:54:23 +0500

    Шахматы, черный слон и пешки готовы (переделаны)

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -25,7 +25,35 @@ public class BishopBlack implements Figure {
     public Cell[] way(Cell source, Cell dest) {
         Cell[] steps = new Cell[0];
         if (Math.abs(source.y - dest.y) == Math.abs(source.x - dest.x)) {
-            steps = new Cell[] { dest };
+            int deltaX = Math.abs(source.x - dest.x);
+            if(source.x < dest.x && source.y < dest.y) {
+                Cell[] tmp = new Cell[deltaX + 1];
+                for (int i = 0; i <= deltaX; i++) {
+                    tmp[i] = Cell.findByXY(source.x + i, source.y + i);
+                }
+                steps = tmp;
+            }
+            if(source.x > dest.x && source.y < dest.y) {
+                Cell[] tmp = new Cell[deltaX + 1];
+                for (int i = 0; i <= deltaX; i++) {
+                    tmp[i] = Cell.findByXY(source.x - i, source.y + i);
+                }
+                steps = tmp;
+            }
+            if(source.x < dest.x && source.y > dest.y) {
+                Cell[] tmp = new Cell[deltaX + 1];
+                for (int i = 0; i <= deltaX; i++) {
+                    tmp[i] = Cell.findByXY(source.x + i, source.y - i);
+                }
+                steps = tmp;
+            }
+            if(source.x > dest.x && source.y > dest.y) {
+                Cell[] tmp = new Cell[deltaX + 1];
+                for (int i = 0; i <= deltaX; i++) {
+                    tmp[i] = Cell.findByXY(source.x - i, source.y - i);
+                }
+                steps = tmp;
+            }
         }
         return steps;
     }
