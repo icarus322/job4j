c785a64ce0eaf77560b74ffd563a76459e15479e
icarus322 <pirat18l@ya.ru>
2019-10-08 13:40:19 +0500

    5.2.1. Реализовать SimpleArray<T>[#186009]
    5.2.1. Реализовать SimpleArray<T>[#186009]
    
    5.2.1. Реализовать SimpleArray<T>[#186009]

--- chapter_005/src/main/java/ru/job4j/generic/SimpleArray.java
+++ chapter_005/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -7,7 +7,6 @@ public class SimpleArray<T> implements Iterable<T> {
 
     private T[] array;
     private int addCounter = 0;
-    private int nextCounter = 0;
 
     public SimpleArray(T[] models) {
         array = models;
@@ -18,48 +17,40 @@ public class SimpleArray<T> implements Iterable<T> {
     }
 
     public void add(T model) {
-        try {
-            this.array[addCounter++] = model;
-        } catch (ArrayIndexOutOfBoundsException e) {
-            throw new RuntimeException(e);
+        if (addCounter >= array.length) {
+            throw new ArrayIndexOutOfBoundsException();
         }
+        this.array[addCounter++] = model;
     }
 
     public void set(int index, T model) {
-        try {
-            this.array[index] = model;
-        } catch (ArrayIndexOutOfBoundsException e) {
-            throw new RuntimeException(e);
+        if (index >= addCounter) {
+            throw new ArrayIndexOutOfBoundsException();
         }
+        this.array[index] = model;
     }
 
     public T get(int index) {
-        try {
-            return this.array[index];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            throw new RuntimeException(e);
+        if (index >= addCounter) {
+            throw new ArrayIndexOutOfBoundsException();
         }
+        return this.array[index];
     }
 
     public void remove(int index) {
-        try {
-            this.array[index] = null;
-            for (int i = 0; i < this.array.length - 1; i++) {
-                if (this.array[i] == null && this.array[i + 1] != null) {
-                    var tmp = this.array[i];
-                    this.array[i] = this.array[i + 1];
-                    this.array[i + 1] = tmp;
-                }
-            }
-            addCounter--;
-        } catch (ArrayIndexOutOfBoundsException e) {
-            throw new RuntimeException(e);
+        if (index >= addCounter) {
+            throw new ArrayIndexOutOfBoundsException();
         }
+        System.arraycopy(this.array, index + 1, this.array, index, addCounter - index - 1);
+        this.array[addCounter - 1] = null;
+        addCounter--;
+
     }
 
     @Override
     public Iterator<T> iterator() {
-        return new Iterator<>(){
+        return new Iterator<>() {
+            private int nextCounter = 0;
 
             @Override
             public boolean hasNext() {
@@ -68,11 +59,10 @@ public class SimpleArray<T> implements Iterable<T> {
 
             @Override
             public T next() {
-                if (nextCounter >= addCounter) {
+                if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
                 return get(nextCounter++);
-
             }
         };
     }
--- chapter_005/src/test/java/ru/job4j/generic/SimpleArrayTest.java
+++ chapter_005/src/test/java/ru/job4j/generic/SimpleArrayTest.java
@@ -2,6 +2,7 @@ package ru.job4j.generic;
 
 import org.junit.Test;
 
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -22,6 +23,9 @@ public class SimpleArrayTest {
     @Test
     public void whenSetItemsThenItemsSet() {
         SimpleArray<Integer> integers = new SimpleArray<>(new Integer[5]);
+        integers.add(-1);
+        integers.add(-1);
+        integers.add(-1);
         integers.set(0, 1);
         integers.set(1, 2);
         integers.set(2, 3);
@@ -48,7 +52,23 @@ public class SimpleArrayTest {
         integers.add(5);
         integers.remove(1);
         Integer[] expect = new Integer[]{1, 3, 4, 5, null};
-        assertThat(expect, is(integers.getArray()));
+        assertThat(integers.getArray(), is(expect));
+    }
+
+    @Test
+    public void whenRemoveItemThenItemRemove1() {
+        SimpleArray<Integer> integers = new SimpleArray<>(new Integer[8]);
+        integers.add(0);
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
+        integers.add(5);
+        integers.add(6);
+        integers.add(7);
+        integers.remove(4);
+        Integer[] expect = new Integer[]{0, 1, 2, 3, 5, 6, 7, null};
+        assertThat(integers.getArray(), is(expect));
     }
 
     @Test(expected = NoSuchElementException.class)
@@ -59,18 +79,19 @@ public class SimpleArrayTest {
         integers.add(3);
         integers.add(4);
         integers.add(5);
-        assertThat(integers.iterator().hasNext(), is(true));
-        assertThat(integers.iterator().next(), is(1));
-        assertThat(integers.iterator().hasNext(), is(true));
-        assertThat(integers.iterator().next(), is(2));
-        assertThat(integers.iterator().hasNext(), is(true));
-        assertThat(integers.iterator().next(), is(3));
-        assertThat(integers.iterator().hasNext(), is(true));
-        assertThat(integers.iterator().next(), is(4));
-        assertThat(integers.iterator().hasNext(), is(true));
-        assertThat(integers.iterator().next(), is(5));
-        assertThat(integers.iterator().hasNext(), is(false));
-        integers.iterator().next();
+        Iterator<Integer> iterator = integers.iterator();
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(1));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(2));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(3));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(4));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(5));
+        assertThat(iterator.hasNext(), is(false));
+        iterator.next();
     }
 
 }
