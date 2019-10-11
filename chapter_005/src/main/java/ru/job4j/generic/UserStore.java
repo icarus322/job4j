fb5069bced14f47450dce4fe9c290701583120d2
icarus322 <pirat18l@ya.ru>
2019-10-11 16:27:55 +0500

    5.2.2. Реализовать Store<T extends Base>[#186010]

--- chapter_005/src/main/java/ru/job4j/generic/UserStore.java
+++ chapter_005/src/main/java/ru/job4j/generic/UserStore.java
@@ -11,15 +11,6 @@ public class UserStore implements Store<User>{
         array = new SimpleArray<>(users);
     }
 
-    public int validateId(String id) {
-        int result;
-        try {
-            result = Integer.valueOf(id);
-        } catch (NumberFormatException e) {
-            throw new RuntimeException("Некорректное значение Id");
-        }
-        return result;
-    }
 
     @Override
     public void add(User model) {
@@ -28,27 +19,18 @@ public class UserStore implements Store<User>{
 
     @Override
     public boolean replace(String id, User model) {
-        if (findId(id) == -1) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
         array.set(findId(id), model);
         return true;
     }
 
     @Override
     public boolean delete(String id) {
-        if (findId(id) == -1) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
         array.remove(findId(id));
         return true;
     }
 
     @Override
     public User findById(String id) {
-        if (findId(id) == -1) {
-            throw new ArrayIndexOutOfBoundsException();
-        }
         return array.get(findId(id));
     }
 
@@ -57,14 +39,14 @@ public class UserStore implements Store<User>{
         int check = 0;
         Iterator<User> it = this.array.iterator();
         while (it.hasNext()) {
-            if (validateId(it.next().getId()) == validateId(id)){
+            if (it.next().getId().equals(id)){
                 check = 1;
                 break;
             }
             counter++;
         }
         if (!it.hasNext() && check != 1) {
-            counter = -1;
+            throw new ArrayIndexOutOfBoundsException("Такого индекса не существует");
         }
 
         return counter;
