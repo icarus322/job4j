c5eeba5f246f3dae9a49be7235cbd6b2e5c34b02
icarus322 <pirat18l@ya.ru>
2019-10-11 16:04:54 +0500

    5.2.2. Реализовать Store<T extends Base>[#186010]

--- chapter_005/src/main/java/ru/job4j/generic/UserStore.java
+++ chapter_005/src/main/java/ru/job4j/generic/UserStore.java
@@ -1,6 +1,7 @@
 package ru.job4j.generic;
 
 
+import java.util.Iterator;
 
 public class UserStore implements Store<User>{
 
@@ -10,6 +11,15 @@ public class UserStore implements Store<User>{
         array = new SimpleArray<>(users);
     }
 
+    public int validateId(String id) {
+        int result;
+        try {
+            result = Integer.valueOf(id);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Некорректное значение Id");
+        }
+        return result;
+    }
 
     @Override
     public void add(User model) {
@@ -18,31 +28,46 @@ public class UserStore implements Store<User>{
 
     @Override
     public boolean replace(String id, User model) {
-        boolean result;
-        try {
-            array.set(Integer.valueOf(id), model);
-            result = true;
-        } catch (ArrayIndexOutOfBoundsException e) {
-            result = false;
+        if (findId(id) == -1) {
+            throw new ArrayIndexOutOfBoundsException();
         }
-        return result;
+        array.set(findId(id), model);
+        return true;
     }
 
     @Override
     public boolean delete(String id) {
-        boolean result;
-        try {
-            array.remove(Integer.valueOf(id));
-            result = true;
-        } catch (ArrayIndexOutOfBoundsException e) {
-            result = false;
+        if (findId(id) == -1) {
+            throw new ArrayIndexOutOfBoundsException();
         }
-        return result;
+        array.remove(findId(id));
+        return true;
     }
 
     @Override
     public User findById(String id) {
-        return array.get(Integer.valueOf(id));
+        if (findId(id) == -1) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return array.get(findId(id));
+    }
+
+    public int findId(String id) {
+        int counter = 0;
+        int check = 0;
+        Iterator<User> it = this.array.iterator();
+        while (it.hasNext()) {
+            if (validateId(it.next().getId()) == validateId(id)){
+                check = 1;
+                break;
+            }
+            counter++;
+        }
+        if (!it.hasNext() && check != 1) {
+            counter = -1;
+        }
+
+        return counter;
     }
 
     public SimpleArray<User> getArray() {
