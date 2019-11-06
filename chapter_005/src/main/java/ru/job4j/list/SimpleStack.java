f4af66c86d4d2d98a77b6366d14c7c5900f08b9a
icarus322 <pirat18l@ya.ru>
2019-11-06 15:59:41 +0500

    5.3.3. Используя контейнер на базе связанного списка создать контейнер Stack[#185986]

--- chapter_005/src/main/java/ru/job4j/list/SimpleStack.java
+++ chapter_005/src/main/java/ru/job4j/list/SimpleStack.java
@@ -2,7 +2,7 @@ package ru.job4j.list;
 
 public class SimpleStack<T> {
 
-    SimpleLinckedList<T> list = new SimpleLinckedList<>();
+    private SimpleLinckedList<T> list = new SimpleLinckedList<>();
 
     public T poll() {
         return list.deleteLast();
